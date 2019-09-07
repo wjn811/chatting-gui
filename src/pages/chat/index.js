73c2eec6e0a43db933513a7c212cfa91f303adf7
ericwang <crazydog.eric@gmail.com>
2019-09-08 00:45:00 +0800

    init

---
+++ src/pages/chat/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col, Card } from 'antd'
+
+class Chat extends Component{
+
+    constructor(args){
+        super(args);
+    }
+
+    render(){
+        return <div>
+            this is chat window;
+        </div>;
+    }
+}
+
+export default Chat
